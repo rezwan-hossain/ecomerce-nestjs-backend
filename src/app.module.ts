316b450dcf304bdcb9c4040ff2bd40ca852ae7bb
rezwan hossain <rezwanvoot@gmail.com>
2026-05-05 14:10:49 +0600

    Add zod adn swagger to the product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { ProductsModule } from './modules/products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {
+export class ProductsService {
   constructor(private prisma: PrismaService) {}
 }
