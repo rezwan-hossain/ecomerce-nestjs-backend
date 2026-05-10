0b013d5b1a79289ec45727901cfd88478b6e3f37
rezwan hossain <rezwanvoot@gmail.com>
2026-05-11 00:49:29 +0600

    Add product variants, options, and images

---
+++ src/modules/categories/dto/categories.dto.ts
@@ -0,0 +1,15 @@
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+
+export const createCategorySchema = z.object({
+  name: z.string().min(2).max(255),
+  slug: z
+    .string()
+    .min(2)
+    .max(255)
+    .regex(/^[a-z0-9-]+$/)
+    .optional(),
+  parentId: z.uuid().optional(),
+});
+
+export class CreateCategoryDto extends createZodDto(createCategorySchema) {}
